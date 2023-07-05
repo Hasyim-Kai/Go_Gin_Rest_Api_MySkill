811e1e35aaab393aba71dd6850e65e8c5af20d22
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-05 10:42:47 +0700

    practice test

---
+++ practice/json_test.go
@@ -0,0 +1,11 @@
+package practice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHitungVolume(t *testing.T) {
+	assert.Equal(t, 20, 30, "perhitungan volume salah")
+}
